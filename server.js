931f7aff1616bfd884a973711379fbeda8928d8c
PETER MUYOMBA <111797748+pmuyomba11@users.noreply.github.com>
2024-05-08 11:41:04 -0400

    basic setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const mongoose = require('mongoose');
+const port = process.env.PORT;
+const colors = require('colors');
+const morgan = require('morgan');
+
+// Middleware
+app.use(express.json());
+app.use(morgan('dev')); // Developer loggers
+
+// DB CONNECTION
+mongoose.connect(process.env.DATABASE_URL)
+
+// DB Success message
+mongoose.connection.on('connected', () => {
+  console.log(`DATABASE CONNECTED.....`.green.bold.inverse);
+  app.listen(port, () => {
+    console.log(`Server running on PORT: ${port}.....`.cyan.bold.inverse);
+  });
+});
+
+// DB Failure message
+mongoose.connection.on('error', (error) => {
+  console.log(`Connection Error: ${error.message}`.red.bold);
+});
